5a640698c7519f7eb0905bfc802c984d053d4c1a
Jakub Ruszil <jakubruszil@gmail.com>
2018-05-27 09:44:49 +0200

    Initial commit

---
+++ src/test/java/pope/two_one_three_seven/TwoOneThreeSevenApplicationTests.java
@@ -0,0 +1,16 @@
+package pope.two_one_three_seven;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TwoOneThreeSevenApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
